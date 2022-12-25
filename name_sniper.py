f7542cbddd45a95a4f1c9fbd9ab9a714b96d29b1
noé m <noe.moyen@epitech.eu>
2022-12-25 16:22:13 +0100

    init commit

--- name_sniper.py
+++ name_sniper.py
@@ -6,8 +6,9 @@ connector = Connector()
 @connector.ready
 
 async def connect(connection):
-    names = ['Faker', 'Huni', 'Ssumday', 'Bang', 'CoreJJ', 'Zven', 'Rekkles', 'Caps', 'Perkz', 'Jankos']
     print("Name Sniper is ready to work !")
+    #Here is exemple of name you can try to snipe:
+    names = ['Faker', 'Huni', 'Ssumday', 'Bang', 'CoreJJ', 'Zven', 'Rekkles', 'Caps', 'Perkz', 'Jankos']
     k = 0
     while True:
         for i in names:
@@ -17,11 +18,10 @@ async def connect(connection):
             changename = await connection.request('get', "/lol-summoner/v1/check-name-availability/" + i)
             changename = await changename.json()
             print("try " + i,  changename)
+            #if the name is found, you can change your name with this code:
             if changename == True:
-                print(i)
                 #response = await connection.request('post', '/lol-summoner/v1/current-summoner/name', data=json.dumps({"name": i}), headers=headers)
-                #response = await response.json()
-                #print("name found !!!!" + response)
+                print("name found !!!!" + i)
                 exit(0)
 
 connector.start()
